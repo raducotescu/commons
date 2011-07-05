60913e66622a43a3c6bec2a242c68e756441067e
Radu Cotescu <radu@cotescu.com>
2011-07-05 11:57:39 +0100

    fixed Exceptions handling for Downloader

--- src/main/java/com/cotescu/radu/commons/Downloader.java
+++ src/main/java/com/cotescu/radu/commons/Downloader.java
@@ -1,6 +1,7 @@
 package com.cotescu.radu.commons;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,8 +15,9 @@ import java.util.Date;
 
 /**
  * This class provides various methods useful for downloading files.
+ * 
  * @author Radu Cotescu
- *
+ * 
  */
 public class Downloader {
 	private final static String UA = "Mozilla/5.0 (X11; Linux x86_64; rv:5.0) Gecko/20100101 Firefox/5.0";
@@ -32,50 +34,49 @@ public class Downloader {
 	 *            a String containing the folder where the folder must be
 	 *            downloaded; if it is null or the empty String ("") the file
 	 *            will be downloaded in the current directory
-	 * @return	a String containing the full path to the downloaded file
+	 * @return a String containing the full path to the downloaded file
+	 * @throws FileNotFoundException
+	 *             if the file specified by the URL is not found
+	 * @throws MalformedURLException
+	 *             if the URL is badly formed
+	 * @throws IOException
+	 *             if the transfer of the file to the specified location cannot
+	 *             be done
 	 */
-	public static String downloadFileFromURL(String from, String where) {
+	public static String downloadFileFromURL(String from, String where)
+			throws FileNotFoundException, MalformedURLException, IOException {
 		String file = null;
-		try {
-			String to = new File("." + File.separator).getCanonicalPath();
-			if (StringUtils.isEmpty(where)) {
-				where = to;
-			}
-			URL url = new URL(from);
-			URLConnection connection = url.openConnection();
-			/**
-			 * some servers check the user agent and if they find something
-			 * suspicious they return HTTP 403 error code
-			 */
-			connection.setRequestProperty("User-Agent", UA);
-			InputStream is = connection.getInputStream();
-			ReadableByteChannel rbc = Channels.newChannel(is);
-			String fullPath = url.getFile();
-			String tmpfile = null;
-			if (StringUtils.isEmpty(fullPath)) {
-				SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
-				tmpfile = sdf.format(new Date());
-			} else if(StringUtils.isEmpty(tmpfile) && fullPath.length() == 1){
-				SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
-				tmpfile = sdf.format(new Date());
-			} else {
-				tmpfile = fullPath.substring(fullPath.lastIndexOf("/") + 1, fullPath.length());
-			}
-			if (where.endsWith(File.separator)) {
-				tmpfile = where + tmpfile;
-			} else {
-				tmpfile = where + File.separator + tmpfile;
-			}
-			FileOutputStream fos = new FileOutputStream(tmpfile);
-			fos.getChannel().transferFrom(rbc, 0, 1 << 24);
-			file = tmpfile;
-		} catch (MalformedURLException e) {
-			System.err.println("Malformed URL...");
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.err.println("Unable to open URL stream...");
-			e.printStackTrace();
+		String to = new File("." + File.separator).getCanonicalPath();
+		if (StringUtils.isEmpty(where)) {
+			where = to;
 		}
+		URL url = new URL(from);
+		URLConnection connection = url.openConnection();
+		/**
+		 * some servers check the user agent and if they find something
+		 * suspicious they return HTTP 403 error code
+		 */
+		connection.setRequestProperty("User-Agent", UA);
+		InputStream is = connection.getInputStream();
+		ReadableByteChannel rbc = Channels.newChannel(is);
+		String fullPath = url.getFile();
+		if (StringUtils.isEmpty(fullPath)) {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+			file = sdf.format(new Date());
+		} else if (StringUtils.isEmpty(file) && fullPath.length() == 1) {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+			file = sdf.format(new Date());
+		} else {
+			file = fullPath.substring(fullPath.lastIndexOf("/") + 1,
+					fullPath.length());
+		}
+		if (where.endsWith(File.separator)) {
+			file = where + file;
+		} else {
+			file = where + File.separator + file;
+		}
+		FileOutputStream fos = new FileOutputStream(file);
+		fos.getChannel().transferFrom(rbc, 0, 1 << 24);
 		return file;
 	}
 }
--- src/test/java/com/cotescu/radu/commons/tests/DownloaderTest.java
+++ src/test/java/com/cotescu/radu/commons/tests/DownloaderTest.java
@@ -1,27 +1,57 @@
 package com.cotescu.radu.commons.tests;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
 
-import org.junit.AfterClass;
+import org.junit.After;
 import org.junit.Test;
 
 import com.cotescu.radu.commons.Downloader;
 
 public class DownloaderTest {
-	
+
 	private static String file;
-	
+
 	@Test
-	public final void testDownloadFileFromURL() {
-		assertNotNull((file = Downloader.downloadFileFromURL("http://radu.cotescu.com/", null)));
+	public final void testDownloadFileFromURLExceptionNotRaised()
+			throws Exception {
+		assertNotNull((file = Downloader.downloadFileFromURL(
+				"http://radu.cotescu.com/", null)));
 	}
-	
-	@AfterClass
-	public static final void tearDown() {
+
+	@Test(expected = FileNotFoundException.class)
+	public final void testDownloadFileFromURLFileNotFoundException()
+			throws Exception {
+		assertNull(Downloader.downloadFileFromURL("http://radu.cotescu.com/nothinghere",
+				null));
+	}
+
+	@Test(expected = MalformedURLException.class)
+	public final void testDownloadFileFromURLMalformedURLException()
+			throws Exception {
+		assertNull(Downloader.downloadFileFromURL("noprotocol://radu.cotescu.com", null));
+	}
+
+	@Test(expected = IOException.class)
+	public final void testDownloadFileFromURLIOException()
+			throws Exception {
+		String root = null;
+		try {
+			root = File.listRoots()[0].getCanonicalPath();
+		} catch (IOException e) {
+			System.err.println("Unable to find root filesystem.");
+		}
+		assertNull(Downloader.downloadFileFromURL("http://radu.cotescu.com", root));
+	}
+
+	@After
+	public final void tearDown() {
 		File f = new File(file);
-		if(f.exists()) {
+		if (f.exists()) {
 			f.delete();
 		}
 	}
