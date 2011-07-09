c2f90b3568234484477712307e1f480bc1251b90
Radu Cotescu <radu@cotescu.com>
2011-07-09 02:29:38 +0100

    refactored code for the PropertiesLoader class so that custom files can be provided

--- src/main/java/com/cotescu/radu/commons/PropertiesLoader.java
+++ src/main/java/com/cotescu/radu/commons/PropertiesLoader.java
@@ -1,6 +1,8 @@
 package com.cotescu.radu.commons;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Properties;
@@ -20,33 +22,46 @@ public class PropertiesLoader {
 	 * @param propertiesFile
 	 *            the file (as String) from which to return the properties
 	 * @return a Properties object containing the read properties
+	 * @throws IOException if the file cannot be opened
 	 * @see Properties
 	 */
-	public static Properties getPropertiesFromFile(String propertiesFile) {
+	public static Properties getPropertiesFromFileInClasspath(
+			String propertiesFile) throws IOException {
 		Properties p = new Properties();
 		ClassLoader loader = PropertiesLoader.class.getClassLoader();
 		if (loader == null) {
 			loader = ClassLoader.getSystemClassLoader();
 		}
 		URL url = loader.getResource(propertiesFile);
-		try {
-			p.load(url.openStream());
-		} catch (IOException e) {
-			System.err.println("Could not load configuration file: "
-					+ propertiesFile);
-		}
+		p.load(url.openStream());
 		return p;
 	}
 
 	/**
-	 * Returns the properties saved in a properties file.
-	 * 
-	 * @param propertiesFile
-	 *            the file from which to return the properties
+	 * Allows to read properties saved in a file stored in different location from the classpath.
+	 * @param propertiesFile the full path to the file from which to read the properties
 	 * @return a Properties object containing the read properties
-	 * @see Properties
+	 * @throws IOException if any IO exception occurs
 	 */
-	public static Properties getPropertiesFromFile(File propertiesFile) {
-		return getPropertiesFromFile(propertiesFile.getName());
+	public static Properties getPropertiesFromFile(String propertiesFile) throws IOException {
+		File f = new File(propertiesFile);
+		return getPropertiesFromFile(f);
+	}
+	
+	/**
+	 * Allows to read properties saved in a file stored in different location from the classpath.
+	 * @param propertiesFile the file from which to read the properties
+	 * @return a Properties object containing the read properties
+	 * @throws IOException if any IO exception occurs
+	 */
+	public static Properties getPropertiesFromFile(File propertiesFile) throws IOException {
+		Properties p = new Properties();
+		if (!propertiesFile.exists()) {
+			throw new FileNotFoundException("File " + propertiesFile.getCanonicalPath()
+					+ " has not been found.");
+		}
+		FileInputStream fis = new FileInputStream(propertiesFile);
+		p.load(fis);
+		return p;
 	}
 }
